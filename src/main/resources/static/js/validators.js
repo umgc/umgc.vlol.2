78c8d9addc8c77be24c58afd7cd30a1d57d531ed
hmbarnes <hbarnes9@student.umgc.edu>
2020-09-13 11:28:06 -0400

    new custom validators

--- src/main/resources/static/js/validators.js
+++ src/main/resources/static/js/validators.js
@@ -6,3 +6,21 @@ window.Parsley.addValidator('rfc3986', {
     en: 'Input contains illegal characters'
   }
 });
+
+window.Parsley.addValidator('social', {
+  validateMultiple: function(value) {
+    return value && value.match(/^\d{3}-?\d{2}-?\d{4}$/g) || false
+  },
+  messages: {
+    en: 'Invalid input'
+  }
+});
+
+window.Parsley.addValidator('dob', {
+  validateMultiple: function(value) {
+    return value && value.match(/(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/) || false
+  },
+  messages: {
+    en: 'Input contains illegal characters'
+  }
+});
