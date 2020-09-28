19fa6579a35d15ba130a9b89454afd367ec3a186
Michael Marcucci <mmarcucci1@student.umgc.edu>
2020-09-28 08:45:43 -0400

    Update QR Code to be in a printable page

--- src/main/java/com/vlol/controller/QRController.java
+++ src/main/java/com/vlol/controller/QRController.java
@@ -14,38 +14,45 @@
  */
 package com.vlol.controller;
 
+import com.vlol.model.User;
 import com.vlol.service.QRCodeService;
+import com.vlol.service.UserService;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+import java.util.Base64;
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/user")
 public class QRController {
 
-  @Autowired private QRCodeService qrCodService;
+  @Autowired private QRCodeService qrCodeService;
 
-  @GetMapping(value = "/qr-code/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
-  public void qrcodeGenerator(HttpServletResponse response, @PathVariable("barcode") String barcode)
-      throws Exception {
-    BufferedImage in = qrCodService.generateQRCodeImage(barcode);
-    ByteArrayOutputStream os = new ByteArrayOutputStream();
-    ImageIO.write(
-        in, "jpeg", os); // Passing: ​(RenderedImage im, String formatName, OutputStream output)
-    InputStream is = new ByteArrayInputStream(os.toByteArray());
-    response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-    IOUtils.copy(is, response.getOutputStream());
+  @Autowired private UserService userService;
+
+  @GetMapping(value = "/qr-code/{id}")
+  public ModelAndView qrcodeGenerator(@PathVariable("id") Long id) {
+    User user = Utils.getIfUserOrAdmin(userService, id, true);
+    if (user == null) return new ModelAndView("redirect:/login");
+
+    ModelAndView mav = new ModelAndView("user/qr-code");
+    Utils.getUserName(userService, mav);
+    try {
+      BufferedImage in = qrCodeService.generateQRCodeImage(id);
+      ByteArrayOutputStream os = new ByteArrayOutputStream();
+      ImageIO.write(in, "jpeg", os);
+
+      mav.addObject("qrImage", Base64.getEncoder().encodeToString(os.toByteArray()));
+    } catch (Exception e) {
+      mav.addObject("error", true);
+    }
+
+    return mav;
   }
-  // ...
-
 }
