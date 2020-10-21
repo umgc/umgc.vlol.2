e472bd4c49523fe3f0f16d49502b06bc528caa4d
Michael Marcucci <mmarcucci1@student.umgc.edu>
2020-10-20 21:51:52 -0400

    Fix saves not saving

--- src/main/resources/static/js/validators.js
+++ src/main/resources/static/js/validators.js
@@ -1,3 +1,5 @@
+window.Parsley.options.excluded = "input[type=button], input[type=submit], input[type=reset], input[type=hidden], [disabled], :hidden, [readonly]";
+
 window.Parsley.addValidator('rfc3986', {
   validateString: function(value) {
     return value && value.match(/^[A-Za-z0-9\s._~:\\/?#\[\]@!$&'()*+,;=-]*$/g) || false
