16b31806b5db5b88ef82c2ade83cc4d686e3a3f1
Andrew Coleman <acoleman81@student.umgc.edu>
2020-10-29 20:34:27 -0400

    Changed passwords for test accounts (admin, provi, agent, parti) to test1234 to conform to 8-character minimum password length

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO appuser (user_id, role_id, first_name, last_name, email, password, date_created, last_login_date, admin_comments, is_account_verified, is_locked, is_email_verified, login_attempt, last_login_attempt) VALUES 
-(1, 1, 'Lorraine', 'Baines', 'parti@vlol.gov', '$2a$10$wbBBPoyyiMNr5sUbInHCM.rrXk8PfbfLTkWJ9KEglBynBFaNSmjsS', '2020-05-20 16:30:00', '2020-06-23 15:10:00', 'Lorraine is a program participant.', true, false, true, 0, '2020-05-20 16:30:00'),
-(2, 2, 'Marty', 'McFly', 'provi@vlol.gov', '$2a$10$wbBBPoyyiMNr5sUbInHCM.rrXk8PfbfLTkWJ9KEglBynBFaNSmjsS', '2020-05-20 18:30:00', '2020-06-23 15:10:00', 'Marty is an emergency medical provider.', true, false, true, 0, '2020-05-20 16:30:00'),
-(3, 1, 'Emmett', 'Brown', 'agent@vlol.gov', '$2a$10$wbBBPoyyiMNr5sUbInHCM.rrXk8PfbfLTkWJ9KEglBynBFaNSmjsS', '2020-05-20 19:30:00', '2020-06-23 15:20:00', 'Emmett is a Lorraine''s agent.', true, false, true, 0, '2020-05-20 16:30:00'),
-(4, 3, 'Seamus', 'McFly', 'admin@vlol.gov', '$2a$10$wbBBPoyyiMNr5sUbInHCM.rrXk8PfbfLTkWJ9KEglBynBFaNSmjsS', '2020-05-20 20:30:00', '2020-06-23 15:30:00', 'Seamus is a system administrator.', true, false, true, 0, '2020-05-20 16:30:00');
+(1, 1, 'Lorraine', 'Baines', 'parti@vlol.gov', '$2a$10$qXUItYa8ljnMnDBH7chJbOvJbwf1qyVVNSZ220YT9.Adbw5BiXZI2', '2020-05-20 16:30:00', '2020-06-23 15:10:00', 'Lorraine is a program participant.', true, false, true, 0, '2020-05-20 16:30:00'),
+(2, 2, 'Marty', 'McFly', 'provi@vlol.gov', '$2a$10$qXUItYa8ljnMnDBH7chJbOvJbwf1qyVVNSZ220YT9.Adbw5BiXZI2', '2020-05-20 18:30:00', '2020-06-23 15:10:00', 'Marty is an emergency medical provider.', true, false, true, 0, '2020-05-20 16:30:00'),
+(3, 1, 'Emmett', 'Brown', 'agent@vlol.gov', '$2a$10$qXUItYa8ljnMnDBH7chJbOvJbwf1qyVVNSZ220YT9.Adbw5BiXZI2', '2020-05-20 19:30:00', '2020-06-23 15:20:00', 'Emmett is a Lorraine''s agent.', true, false, true, 0, '2020-05-20 16:30:00'),
+(4, 3, 'Seamus', 'McFly', 'admin@vlol.gov', '$2a$10$qXUItYa8ljnMnDBH7chJbOvJbwf1qyVVNSZ220YT9.Adbw5BiXZI2', '2020-05-20 20:30:00', '2020-06-23 15:30:00', 'Seamus is a system administrator.', true, false, true, 0, '2020-05-20 16:30:00');
 
 INSERT INTO user_info (user_id, dob, ssn, street_address, city, us_state, zipcode, phone, ins_company, ins_policy_no, poc_name, poc_phone, doctor_name, doctor_phone, user_comments, is_deceased) VALUES 
 (1, '2015-10-21', '123456789', '1711 Bushnell Ave', 'South Pasadena', 'CA', '91030', '9165555555', 'Aetna', '123456789', 'Marty McFly', '9168423138',  'Dr. Brown', '9165551234', '', false),
